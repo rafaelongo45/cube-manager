e5343155ef971da6425dc800a949d515f52fb277
Rafael Longo <rafaelongo45@gmail.com>
2022-09-25 18:00:47 -0300

    Fix: Rewrites renderization of icons

--- components/Footer/RenderIcon.js
+++ components/Footer/RenderIcon.js
@@ -1,46 +1,46 @@
-import { StyleSheet } from "react-native-web";
+import { Pressable } from "react-native";
 import Icon from "react-native-vector-icons/Ionicons.js";
 import styled from "styled-components";
 
 function RenderIcon({ circle, stringName, iconClicked, setIconClicked }) {
   return (
-    <>
-      {circle ? (
-        <PlusWrapper>
-          <Icon name={stringName} size={55} color="#fff" />
-        </PlusWrapper>
-      ) : (
-        <IconWrapper>
-          <Icon name={stringName} size={35} color="#fff" />
-        </IconWrapper>
-      )}
-    </>
+    <IconWrapper circle={circle}>
+      <Pressable onPress={() => setIconClicked(stringName)}>
+        <IconBg
+          circle={circle}
+          iconClicked={iconClicked}
+          stringName={stringName}
+        >
+          <Icon name={stringName} size={circle ? 45 : 35} color="#fff" />
+        </IconBg>
+      </Pressable>
+    </IconWrapper>
   );
 }
 export default RenderIcon;
 
 const IconWrapper = styled.View`
+  position: ${({ circle }) => (circle ? "relative" : "static")};
+  top: ${({ circle }) => (circle ? "-50px" : "0px")};
   display: flex;
-  background-color: red;
   justify-content: center;
   align-items: center;
-  width: 11%;
-  height: 50px;
-  color: white;
-  padding: 5px 20px;
-  border-radius: 16px;
-  background-color: rgba(255, 255, 255, 0.25);
+  width: ${({ circle }) => (circle ? "70px" : "11%")};
+  border-radius: ${({ circle }) => (circle ? "50px" : "16px")};
+  height: ${({ circle }) => (circle ? "70px" : "50px")};
+  background-color: #3c3c3c;
 `;
 
-const PlusWrapper = styled.View`
-  position: relative;
+const IconBg = styled.View`
+  position: ${({ circle }) => (circle ? "relative" : "static")};
+  top: ${({ circle }) => (circle ? "-4px" : "0px")};
   display: flex;
   justify-content: center;
   align-items: center;
-  padding-left: 4px;
-  top: -50px;
-  width: 70px;
-  height: 70px;
-  border-radius: 50px;
-  background-color: #3c3c3c;
+  width: ${({ circle }) => (circle ? "50px" : "100%")};
+  height: ${({ circle }) => (circle ? "50px" : "50px")};
+  padding: ${({ circle }) => (circle ? "0 0 10px 3px" : "5px 20px")};
+  background-color: ${({ iconClicked, stringName }) =>
+    iconClicked === stringName ? "rgba(255, 255, 255, 0.25)" : "#3c3c3c"};
+  border-radius: ${({ circle }) => (circle ? "50px" : "16px")};
 `;
